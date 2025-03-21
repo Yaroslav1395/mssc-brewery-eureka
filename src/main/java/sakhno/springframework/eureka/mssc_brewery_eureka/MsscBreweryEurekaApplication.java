bd3dadb0694666b5edf9306b83031500c4674137
Yarnercom <a.doroev@mail.ru>
2025-03-21 18:37:45 +0600

    1 ком

---
+++ src/main/java/sakhno/springframework/eureka/mssc_brewery_eureka/MsscBreweryEurekaApplication.java
@@ -0,0 +1,15 @@
+package sakhno.springframework.eureka.mssc_brewery_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsscBreweryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sakhno/springframework/eureka/mssc_brewery_eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package sakhno.springframework.eureka.mssc_brewery_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
